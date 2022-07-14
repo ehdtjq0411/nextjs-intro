ad040c7ab459306f627544b28801b48358544be7
ehdtjq0411 <ehdtjq0411@gmail.com>
2022-07-14 10:34:35 +0900

    nextjs-intro

---
+++ components/NavBar.js
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router';
+
+export default function NavBar(){
+    const router = useRouter();
+
+    return (
+        <nav>
+            <Link href="/">
+                <a className={router.pathname === "/" ? "active" : ""}>HOME</a>
+            </Link>
+            <Link href="/about">
+                <a className={router.pathname === "/about" ? "active" : ""}>About</a>
+            </Link>
+            <style jsx>{`
+                nav {
+                    background-color : white;
+                }
+                a {
+                    text-decoration : none;
+                }
+                .active {
+                    color : tomato;
+                }
+            `}</style>
+        </nav>
+    );
+}
---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import NavBar from "../components/NavBar2";
+
+export default function Potato(){
+    return (
+        <div>
+            <h1>About</h1>
+        </div>
+    );
+}
